a0f88c9a100ca95c6746eed3bee1dafd837bdb09
undying.muse <161160763+undying-muse@users.noreply.github.com>
2024-06-22 18:49:03 +0300

    Update new.java

---
+++ new.java
@@ -0,0 +1,85 @@
+print(" ")
+
+//As Java is close to C++ and C#, it makes it easy for programmers to switch to Java or vice versa
+
+//In Java, every application begins with a class name, and that class must match the filename.
+
+//Let's create our first Java file, called Main.java, which can be done in any text editor (like Notepad).
+
+//The file should contain a "Hello World" message, which is written with the following code:
+
+//Main.java
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+//Save the code in Notepad as "Main.java". Open Command Prompt (cmd.exe), navigate to the directory where you saved your file, and type "javac Main.java":
+
+
+//For now, just remember that every Java program has a 
+//class name which must match the filename, and that every 
+//program must contain the main() method.
+
+//Yeah I wrote the stuff the w3 schools gave me but I have literally no idea what
+//I should be doing.
+
+//Note: The curly braces {} marks the beginning and the end of a block of code.
+
+//System is a built-in Java class that contains useful members, 
+//such as out, which is short for "output". The println() method, short 
+//for "print line", is used to print a value to the screen (or a file).
+
+//Don't worry too much about System, out and println(). Just know 
+//that you need them together to print stuff to the screen.
+
+//You should also note that each code statement must end with a 
+//semicolon (;).
+
+//Ohhh so thats where the semicolon obsession comes from. I didn't know that Java 
+//NEEDED the semicolons.
+
+
+public class MyClass {
+  public static void main(String[] args) {
+   System.out.println("Hello World");
+  }
+}
+
+
+//My first command!! 
+
+//And funny story I mixed that with printIn. Yeah... dyslexia.
+//Anyways back to learning.
+
+System.out.println("Hello World!");
+System.out.println("I am learning Java.");
+System.out.println("It is awesome!");
+Semicolons!!!
+//I'm having fun with this Afdkfhdskf
+
+//note: must not forget the quotes when using println command
+//There is also a print() method, which is similar to println().
+
+System.out.print("Hello World! ");
+System.out.print("What's up?");
+
+//the print function prints whatever's inside on the same line as println
+//Note that we add an extra space (after "Hello World!" in the example above), for better readability.
+//oooooo so the print doesnt leave any space behind as well. cool!
+
+
+//Seems like we can print the numbers and equations as well.
+
+//Look!
+System.out.print(200);
+System.out.print(5*10);
+System.out.print(2+3);
+
+//I learned how to use comments!!
+//It was a little disorienting because I was used to the hash symbol instead of the
+//two slashes.
+
+System.out.print(2+3); //Note, they work in the middle of llines!!
